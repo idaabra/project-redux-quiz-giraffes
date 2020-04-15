33dba178949a683d469033f4488a3bb48f375b16
VickBe <32632292+VickBe@users.noreply.github.com>
2020-04-15 11:33:10 +0200

    added fonts

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -2,45 +2,52 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { quiz } from 'reducers/quiz'
 import { Progress } from './Progress'
+import './quiz.css'
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuesionIndex])
   const dispatch = useDispatch()
-
-
+  
+  
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
-
+  
   const handleClick = (index) => {
     dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }))
   }
-
+  
   return (
-    <main>
+    <main className="currentQuestion">
       <img src={question.image} alt="Giraffe" />
-      <h1>Question: {question.questionText}</h1>
+      <h1 className="questionText">Question: {question.questionText}</h1>
+      <div className="answerContainer">
       {question.options.map((answer, index) => {
         return (
-          <button key={index}
-            onClick={() => handleClick(index)}>
-            {answer}
+          
+          <button className="answerOption" 
+          key={index}
+          onClick={() => handleClick(index)}>
+          {answer}
           </button>
-        )
-      })}
-      <button onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
+          
+          )
+        })}
+        </div>
+        <button className="moveOn" onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
         NEXT
-      </button>
-      <Progress/>
-    </main>
-  )
-}
-
-
-
-
-
-
-
-
-
+        </button>
+        <Progress/>
+      </main>
+      )
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
